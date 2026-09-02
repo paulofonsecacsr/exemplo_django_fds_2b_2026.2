3638fcc8f0a1a7b94477bd1fee61377cbe609a71
Paulo <pffs@cesar.school>
2026-09-02 09:35:52 -0300

    Adiciona 'detalhe.html'

--- project/urls.py
+++ project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("hello/", include("hello.urls")),
     path("forum/", include("forum.urls")),
-    path("", RedirectView.as_view(url="/forum/", permanent=False), nome="home")
+    path("", RedirectView.as_view(url="/forum/", permanent=False), name="home")
 ]
